9976b40eec281d4b57e4998301d315cd472cced8
Ritika <rishikajangir123@outlook.com>
2026-09-06 15:24:39 +0530

    Update website layout with farmer registration

--- frontend/.next/server/middleware-build-manifest.js
+++ frontend/.next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-fef3eb2633326c29.js","static/chunks/fd9d1056-b269926aa472f095.js","static/chunks/117-abca80f805db3ab7.js","static/chunks/main-app-1d3010c366a3deb3.js"],pages:{"/_app":["static/chunks/webpack-fef3eb2633326c29.js","static/chunks/framework-00a8ba1a63cfdc9e.js","static/chunks/main-1348566cfb77854d.js","static/chunks/pages/_app-15e2daefa259f0b5.js"],"/_error":["static/chunks/webpack-fef3eb2633326c29.js","static/chunks/framework-00a8ba1a63cfdc9e.js","static/chunks/main-1348566cfb77854d.js","static/chunks/pages/_error-28b803cb2479b966.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
